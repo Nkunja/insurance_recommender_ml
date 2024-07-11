813fb2076b8bc6e4f6caa6cc9b767108650d60d2
Nkunja <munene01denis@gmail.com>
2024-07-11 14:04:14 +0300

    Credit score

--- recommender/forms.py
+++ recommender/forms.py
@@ -5,3 +5,4 @@ class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = ['age', 'income', 'credit_score', 'gender', 'occupation', 'marital_status', 'number_of_dependents']
+        # credit score should come from model
